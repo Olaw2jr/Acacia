4c362b0c26c8caff598c2c49bc2e5bf513bf5434
Olaw2jr <oscar.eugine@gmail.com>
2014-12-26 12:53:32 +0300

    Initial commit, cloned a fresh laravel

---
+++ public/wordpress/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
+/** For security issues go to /wordpress/index.php file and put this first, to redirect to the Admin login page: */
+header("Location: ./wp-admin");
+exit();
